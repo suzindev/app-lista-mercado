91188693b8e22ac1b2ae9704d7505a2ea0d60e84
Fernando Suzin <suzin.dev@gmail.com>
2024-07-03 16:13:57 -0300

    Projeto criado

---
+++ backend/src/main/java/br/com/suzintech/listamercado/AppListaMercadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.suzintech.listamercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppListaMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppListaMercadoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/suzintech/listamercado/AppListaMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.suzintech.listamercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppListaMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
